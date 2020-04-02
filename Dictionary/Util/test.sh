9d8040774199f7a0f81a7cf9c70360dd0247a65e
devrath <devrath.ad@cybernetyx.com>
2020-04-02 13:51:15 +0530

    Removed un-nessary test files

--- Dictionary/Util/test.sh
+++
@@ -1,6 +0,0 @@
-JUNIT=/usr/share/java/junit.jar
-test -r "$JUNIT" || JUNIT=/usr/share/junit/lib/junit.jar
-javac -g src/com/hughes/util/*.java src/com/hughes/util/raf/*.java test/com/hughes/util/*.java test/com/hughes/util/raf/*.java -classpath "$JUNIT"
-java -classpath "src:test:$JUNIT" junit.textui.TestRunner com.hughes.util.StringUtilTest
-java -classpath "src:test:$JUNIT" junit.textui.TestRunner com.hughes.android.dictionary.util.raf.UniformRAFListTest
-java -classpath "src:test:$JUNIT" junit.textui.TestRunner com.hughes.android.dictionary.util.raf.RAFListTest
