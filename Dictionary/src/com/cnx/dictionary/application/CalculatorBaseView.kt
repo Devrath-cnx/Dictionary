bed189e194fa2383949c3e6ba6396baa0984bbec
devrath <devrath.ad@cybernetyx.com>
2020-04-02 14:22:02 +0530

    Added custom view

---
+++ Dictionary/src/com/cnx/dictionary/application/CalculatorBaseView.kt
@@ -0,0 +1,37 @@
+package com.cnx.dictionary.application
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import com.cnx.dictionarytool.R
+
+class CalculatorBaseView : FrameLayout {
+
+
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        View.inflate(context, R.layout.fragment_base_dictionary, this)
+
+
+    }
+
+
+
+}
