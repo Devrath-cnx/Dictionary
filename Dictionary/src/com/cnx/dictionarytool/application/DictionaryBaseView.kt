97458f892f977e391536ee07cc0559d1021ffed1
devrath <devrath.ad@cybernetyx.com>
2020-04-04 21:21:23 +0530

    Comitted initial state

--- Dictionary/src/com/cnx/dictionarytool/application/DictionaryBaseView.kt
+++ Dictionary/src/com/cnx/dictionarytool/application/DictionaryBaseView.kt
@@ -5,10 +5,11 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import com.cnx.dictionarytool.R
+import com.cnx.dictionarytool.library.activities.DictionaryApplication
 
-class CalculatorBaseView : FrameLayout {
+class DictionaryBaseView : FrameLayout {
 
-
+    private var application: DictionaryApplication? = null
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -28,10 +29,15 @@ class CalculatorBaseView : FrameLayout {
 
     private fun init(context: Context) {
         View.inflate(context, R.layout.fragment_base_dictionary, this)
-
-
+        initOnCreate(context)
     }
 
+
+
+    private fun initOnCreate(context: Context) {
+        DictionaryApplication.INSTANCE.init(context)
+        application = DictionaryApplication.INSTANCE
+    }
 
 
 }
