32ea687e449b353ce5fdb8b5619ed2f7e2db9a30
devrath <devrath.ad@cybernetyx.com>
2020-04-10 07:33:06 +0530

    Added maven for the library

--- Dictionary/src/com/cnx/dictionarytool/utils/CustTxtVwPoppinsRegular.kt
+++ Dictionary/src/com/cnx/dictionarytool/utils/CustTxtVwPoppinsRegular.kt
@@ -1,11 +1,13 @@
 package com.cnx.dictionarytool.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.widget.TextView
 import com.cnx.dictionarytool.R
 
+@SuppressLint("AppCompatCustomView")
 class CustTxtVwPoppinsRegular (context: Context?, attrs: AttributeSet?) : TextView(context, attrs) {
     init {
         val typeface = Typeface.createFromAsset(getContext().assets, getContext().resources.getString(R.string.fontRegular))
